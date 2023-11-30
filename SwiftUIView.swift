037a6ab0d5c5b3be4989555a7a13362c356b2ebb
Muna <muai0466@gmail.com>
2023-11-30 11:08:42 +0300

    added a file

---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Najd_app
+//
+//  Created by Muna Aiman Al-hajj on 16/05/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
